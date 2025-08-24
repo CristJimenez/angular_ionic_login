3914c971aee0841b147dca639514963e5668d679
Cristian <123cristianjose@gmail.com>
2025-08-24 17:44:59 -0500

    page login and register

---
+++ src/app/pages/register/register.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+  standalone: false,
+})
+export class RegisterPage implements OnInit {
+  public name!: FormControl;
+  public lastName!: FormControl;
+  public email!: FormControl;
+  public password!: FormControl;
+  public registerForm!: FormGroup;
+
+  constructor() {
+    this.initForm();
+  }
+
+  ngOnInit() {}
+
+  private initForm() {
+    this.name = new FormControl('', [Validators.required]);
+    this.lastName = new FormControl('', [Validators.required]);
+    this.email = new FormControl('', [Validators.required, Validators.email, Validators.minLength(3)]);
+    this.password = new FormControl('', [Validators.required]);
+    this.registerForm = new FormGroup({
+      name: this.name,
+      lastName: this.lastName,
+      email: this.email,
+      password: this.password,
+    });
+  }
+
+}
